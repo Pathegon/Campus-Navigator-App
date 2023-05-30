b8d832b263d64a070f218d2420bc9e998396c47d
Mango <63511092+Pathegon@users.noreply.github.com>
2023-05-30 06:39:36 +0500

    Added Armacost Description

--- CampusNavigator/screens/HomeScreen.js
+++ CampusNavigator/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { View, Text, TextInput, Image, StyleSheet } from 'react-native';
 
 const Screen2 = () => {
   return (
@@ -10,6 +10,16 @@ const Screen2 = () => {
         A complete solution to getting lost!
       </Text>
       <TextInput style={styles.searchBox} placeholder="Search" />
+
+      <View style={styles.popularLocationContainer}>
+        <Image source={require('./armacost_building.jpg')} style={styles.locationImage} />
+        <Text style={styles.locationTitle}>Armacost Building</Text>
+        <Text style={styles.locationDescription}>
+          The Armacost Science Building houses the departments of natural and social sciences at FCC.
+          It offers state of the art labs & research facilities including a natural history museum.
+        </Text>
+      </View>
+
       {/* Rest of the screen content */}
     </View>
   );
@@ -43,6 +53,26 @@ const styles = StyleSheet.create({
     borderColor: '#CCCCCC',
     borderRadius: 8,
     paddingHorizontal: 10,
+  },
+  popularLocationContainer: {
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  locationImage: {
+    width: 200,
+    height: 150,
+    marginBottom: 10,
+  },
+  locationTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#000000',
+    marginBottom: 5,
+  },
+  locationDescription: {
+    fontSize: 14,
+    color: '#000000',
+    textAlign: 'center',
   },
 });
 
