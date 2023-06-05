7f981d9e6e39c4d7bed255cb55f5568d307cb2a9
Saad Muaaffar <63511092+Pathegon@users.noreply.github.com>
2023-06-06 03:25:02 +0500

    Revert "Revert "Revert "Merge branch 'main' of https://github.com/Pathegon/Campus-Navigator-App"""
    This reverts commit 39579dae9a15bce12862cef2f361ba4de73d7f09.

--- campusnavfccu/App.tsx
+++ campusnavfccu/App.tsx
@@ -1,29 +1,9 @@
-// App.tsx
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import Test from './Test';
-import ScreenWrapper from './components/ScreenWrapper';
-
-const Stack = createStackNavigator();
+import HomeScreen from './Screens/HomeScreen';
 
 const App: React.FC = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Test">
-        <Stack.Screen
-          name="Test"
-          component={(props) => (
-            <ScreenWrapper>
-              <Test {...props} />
-            </ScreenWrapper>
-          )}
-          options={{ headerShown: false }}
-        />
-        {/* Add more screens here... */}
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <HomeScreen />;
 };
 
 export default App;
--- campusnavfccu/Test.tsx
+++ campusnavfccu/Test.tsx
@@ -4,52 +4,51 @@ import { ScrollView, StyleSheet } from 'react-native';
 import LightButton from './components/LightButton';
 import DarkButton from './components/DarkButton';
 import TextBox from './components/TextBox';
-import PopularLocationsTile from './components/tiles/PopularLocationsTile';
-import SearchedLocationsTile from './components/tiles/SearchedLocationsTile';
-import BigTiles from './components/tiles/BigTiles';
-import BigTilesWithLargeDescription from './components/tiles/BigTilesWithLargeDescription';
-import ReviewsAndTestimonials from './components/ReviewsAndTestimonials';
-import ProfileHeader from './components/ProfileHeader';
-import Header from './components/Header';
+import PopularLocations from './components/PopularLocations';
+import SearchedLocations from './components/SearchedLocations';
+import AccountInfo from './components/AccountInfo';
+
 const Test: React.FC = () => {
-  const testimonials = [
-    // ...testimonials data...
+  const popularLocations = [
+    {
+      image: require('./assets/images/sample.jpg'),
+      title: 'Popular Location 1',
+      likes: 100,
+    },
+    {
+      image: require('./assets/images/sample.jpg'),
+      title: 'Popular Location 2',
+      likes: 200,
+    },
+    // Add more popular locations here...
+  ];
+
+  const searchedLocations = [
+    {
+      image: require('./assets/images/sample.jpg'),
+      title: 'Searched Location 1',
+      category: 'Category 1',
+      address: 'Address 1',
+      reviews: 100,
+    },
+    {
+      image: require('./assets/images/sample.jpg'),
+      title: 'Searched Location 2',
+      category: 'Category 2',
+      address: 'Address 2',
+      reviews: 200,
+    },
+    // Add more searched locations here...
   ];
 
   return (
     <ScrollView style={styles.container}>
-      <ProfileHeader
-        profileImage={require('./assets/images/sample.jpg')}
-        profileName="User Name"
-        tagline="This is a tagline."
-      />
       <LightButton onPress={() => {}} title="Light Button" />
       <DarkButton onPress={() => {}} title="Dark Button" />
       <TextBox onChangeText={(text) => {}} placeholder="Enter text" />
-      <PopularLocationsTile
-        image={require('./assets/images/sample.jpg')}
-        title="Popular Location"
-        likes={100}
-        onLike={() => {}}
-      />
-      <BigTiles
-        image={require('./assets/images/sample.jpg')}
-        title="Big Tile"
-        description="A large reptile with four legs, two arms, a long tail, a skin covered in thick, large scales in orange, red, blue and green, a wide mouth, 2 small eyes, an almost hidden nose and virtually invisible ears."
-      />
-      <SearchedLocationsTile
-        title="Searched Location"
-        category="Category"
-        address="Address"
-        reviews={100}
-      />
-      <BigTilesWithLargeDescription
-        image={require('./assets/images/sample.jpg')}
-        title="Big Tile with Large Description"
-        description="Large Description"
-      />
-      <ReviewsAndTestimonials testimonials={testimonials} layout="horizontal" />
-      <ReviewsAndTestimonials testimonials={testimonials} layout="vertical" />
+      <AccountInfo />
+      <PopularLocations locations={popularLocations} />
+      <SearchedLocations locations={searchedLocations} />
     </ScrollView>
   );
 };
